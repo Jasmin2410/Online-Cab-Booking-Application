9a6a59dda28d6c9dab3c8af11f73a65385965e96
Nabanit08 <navy190899@gmail.com>
2023-05-07 13:12:54 +0530

    Day3

--- OnlineCabBookingApplication/src/main/java/com/masai/model/Customer.java
+++ OnlineCabBookingApplication/src/main/java/com/masai/model/Customer.java
@@ -3,7 +3,29 @@ package com.masai.model;
 import jakarta.persistence.Entity;
 
 @Entity
-public class Customer {
+public class Customer extends AbstractUser {
+@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + "]";
+	}
+
+public Customer(Integer customerId) {
+		super();
+		this.customerId = customerId;
+	}
+
+public Customer(String userName, String password, String address, String mobileNumber, String email) {
+	super(userName, password, address, mobileNumber, email);
+}
+
+public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
 private Integer customerId;
 
 
--- OnlineCabBookingApplication/src/main/java/com/masai/service/CustomerService.java
+++ OnlineCabBookingApplication/src/main/java/com/masai/service/CustomerService.java
@@ -1,5 +1,14 @@
 package com.masai.service;
 
-public interface CustomerService {
+import java.util.List;
 
+import com.masai.model.Customer;
+
+public interface CustomerService {
+public Customer insertCustomer(Customer customer);
+public Customer updateCustomer(Customer customer);
+public Customer deleteCustomer(Integer id);
+public List<Customer> veiwCustomers();
+public Customer viewCustomer(int customerId);
+public Customer validateCustomer(String username,String password);
 }
